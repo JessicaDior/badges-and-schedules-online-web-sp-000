0ca098c531cdedc32a5ef75eb37ff178227ee2dd
Jc Dior <jessicaclairdior@gmail.com>
2020-08-25 23:33:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,7 @@ end
 def assign_rooms(attendees)
   array = []
   attendees.each_with_index do |name, room|
-    array<<"Hello, #{name}! You'll be assigned to room #{room}!"
+    array<<"Hello, #{name}! You'll be assigned to room #{room+1}!"
   end
     return array
 end
