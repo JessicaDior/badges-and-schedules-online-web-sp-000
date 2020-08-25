16a03745736569e6da301c95f69ef58472ae65d1
Jc Dior <jessicaclairdior@gmail.com>
2020-08-25 23:42:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,7 @@ def printer(attendees)
   batch_badge_creator(attendees).each do |names|
     puts names
   end
-  assign_rooms(attendees).each do |room| 
+  assign_rooms(attendees).each do |room|
     puts room
   end
 end
